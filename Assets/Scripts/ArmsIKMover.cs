4b587dfb8ef158da60e36fa9a5b337155bd3c459
Auctioneer99 <rus_zalil_99@mail.ru>
2022-06-14 19:28:10 +0500

    feat: mouse following (not working)

---
+++ Assets/Scripts/ArmsIKMover.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    public class HumanBone
+    {
+        public HumanBodyBones Bone;
+        public float Weight = 1.0f;
+    }
+
+    public class ArmsIKMover : MonoBehaviour
+    {
+        [SerializeField]
+        protected Animator animator;
+
+        public Transform target = null;
+        public Transform aim = null;
+
+        public int iterations = 10;
+        public float weight = 1.0f;
+
+        public HumanBone[] humanBones;
+        Transform[] boneTransforms;
+
+        private void Start()
+        {
+            boneTransforms = humanBones.Select(b => animator.GetBoneTransform(b.Bone)).ToArray();
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 targetPosition = target.position;
+            for(int i = 0; i < iterations; i++)
+            {
+                for (int b = 0; b < boneTransforms.Length; b++)
+                {
+                    Transform bone = boneTransforms[b];
+                    float boneWeight = humanBones[b].Weight * weight;
+                    AimAtTarget(bone, targetPosition, boneWeight);
+                }
+            }
+        }
+
+        private void AimAtTarget(Transform bone, Vector3 targetPosition, float weight)
+        {
+            Vector3 aimDirection = aim.forward;
+            Vector3 targetDirection = targetPosition - aim.position;
+            Quaternion aimTowards = Quaternion.FromToRotation(aimDirection, targetDirection);
+            Quaternion blendedRotation = Quaternion.Slerp(bone.rotation, aimTowards, weight);
+            bone.rotation = blendedRotation;
+        }
+    }
+}
--- Assets/Scripts/MouseInput.cs
+++ Assets/Scripts/MouseInput.cs
@@ -9,19 +9,35 @@ namespace Assets.Scripts
         [SerializeField] private GameObject _player;
         [SerializeField] private GameObject _bulletSource;
         [SerializeField] private float _projectileSpeed;
+        [SerializeField] private Camera _camera;
+        [SerializeField] private Transform _armsTarget;
+        [SerializeField] private float _distanceFromObject;
+        [SerializeField] private GameObject _lol;
+        [SerializeField] private GameObject _lol2;
 
         private void Update()
         {
+            var mousePos = Input.mousePosition;
+            mousePos.z = _distanceFromObject;
+
+            Vector3 pos = _camera.ScreenToWorldPoint(mousePos);
+
+            var delta = pos - _bulletSource.transform.position;
+
+            var angle = Quaternion.FromToRotation(Vector3.up, delta);
+            var eulers = angle.eulerAngles;
+            eulers.y = 0;
+            eulers.x = 0;
+            _armsTarget.rotation = Quaternion.Euler(eulers);
+
             if (Input.GetMouseButtonDown(0) && Time.timeScale > 0)
             {
-                Vector3 deltaVector = Input.mousePosition;
-                deltaVector.x = deltaVector.x / (Screen.width / 2) - 1;
-                deltaVector.y = deltaVector.y / (Screen.height / 2) - 1;
-                deltaVector.z = 0;
-
                 var projectile = Instantiate(_projectilePrefab, _bulletSource.transform.position, Quaternion.identity);
-                projectile.Init(deltaVector.normalized * _projectileSpeed , _player);
+                projectile.Init(eulers.normalized * _projectileSpeed , _player);
+                Debug.Log(eulers);
             }
+            _lol2.transform.position = _lol.transform.position;
+            Debug.Log(_lol2.transform.position);
         }
     }
 }
