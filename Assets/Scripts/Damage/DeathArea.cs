8067d01e23c0c9a7477fa6be6fc970dc6d4bc8b9
Auctioneer99 <rus_zalil_99@mail.ru>
2022-06-15 13:41:55 +0500

    feat: danger zones

---
+++ Assets/Scripts/Damage/DeathArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Damage
+{
+    internal class DeathArea : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            var health = other.GetComponent<AHealth>();
+            if (health != null)
+            {
+                health.TakeDamage();
+            }
+        }
+    }
+}
