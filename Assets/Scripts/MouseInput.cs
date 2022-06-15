c179e3e83374cfa9bee82b7ddc64600292790662
Auctioneer99 <rus_zalil_99@mail.ru>
2022-06-15 12:34:09 +0500

    fix: bullet vector

--- Assets/Scripts/MouseInput.cs
+++ Assets/Scripts/MouseInput.cs
@@ -12,8 +12,6 @@ namespace Assets.Scripts
         [SerializeField] private Camera _camera;
         [SerializeField] private Transform _armsTarget;
         [SerializeField] private float _distanceFromObject;
-        [SerializeField] private GameObject _lol;
-        [SerializeField] private GameObject _lol2;
 
         private void Update()
         {
@@ -21,23 +19,16 @@ namespace Assets.Scripts
             mousePos.z = _distanceFromObject;
 
             Vector3 pos = _camera.ScreenToWorldPoint(mousePos);
+            _armsTarget.position = pos;
+
 
             var delta = pos - _bulletSource.transform.position;
-
-            var angle = Quaternion.FromToRotation(Vector3.up, delta);
-            var eulers = angle.eulerAngles;
-            eulers.y = 0;
-            eulers.x = 0;
-            _armsTarget.rotation = Quaternion.Euler(eulers);
-
+            delta.z = 0;
             if (Input.GetMouseButtonDown(0) && Time.timeScale > 0)
             {
                 var projectile = Instantiate(_projectilePrefab, _bulletSource.transform.position, Quaternion.identity);
-                projectile.Init(eulers.normalized * _projectileSpeed , _player);
-                Debug.Log(eulers);
+                projectile.Init(delta.normalized * _projectileSpeed , _player);
             }
-            _lol2.transform.position = _lol.transform.position;
-            Debug.Log(_lol2.transform.position);
         }
     }
 }
